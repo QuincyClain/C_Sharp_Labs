c7a42d352ee1f649e57551f528610dd27f6a2dfe
QuincyClain <79206316+QuincyClain@users.noreply.github.com>
2021-03-12 23:35:40 +0300

    Add files via upload

---
+++ Second Lab/Лаба 2/Лаба 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Лаба_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Random Rand = new Random();
+            Random Rnd = new Random();
+            int value = Rand.Next(1, 5);
+            char[] Str = new char[value];
+            char[] Str1 = "qwertyuiopasdfghjklzxcvbnm".ToCharArray() ;
+            for (int i = 0; i < value; i++)
+            {
+                Str[i] = Str1[Rnd.Next(0, 25)];
+            }
+            Console.WriteLine(Str);
+            Console.ReadKey();
+        }
+
+    }
+}
