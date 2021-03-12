0c031220c39cfcb81aa1da48beda63e126ec86b1
QuincyClain <79206316+QuincyClain@users.noreply.github.com>
2021-03-12 22:50:35 +0300

    Add files via upload

---
+++ Second Lab/Лаба 2(3)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace data
+{
+    class Program
+    {
+        static void Calculate(string str)
+        {
+            int[] amount = new int[10];
+            for (int i = 0; i < 10; i++) { 
+                amount[i] = 0;
+            }
+            for (int i = 0; i < str.Length; i++) { 
+                if (str[i] >= '0' && str[i] <= '9') { 
+                    amount[str[i] - 48] += 1;
+                }
+            }
+            for (int i = 0; i < 10; i++) { 
+                Console.WriteLine($"Number {i}:" + amount[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string FirstDate = DateTime.Now.ToString("d");
+            Console.WriteLine(FirstDate);
+            Calculate(FirstDate);
+            string SecondDate = DateTime.Now.ToString("F");
+            Console.WriteLine(SecondDate);
+            Calculate(SecondDate);
+            Console.ReadKey();
+        }
+    }
+}
