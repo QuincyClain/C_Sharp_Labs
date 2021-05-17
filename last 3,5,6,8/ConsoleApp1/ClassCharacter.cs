bf581c4f21c4c1478fc1a5c97a00643298d4f66b
QuincyClain <79206316+QuincyClain@users.noreply.github.com>
2021-05-17 21:32:33 +0300

    Add files via upload

---
+++ last 3,5,6,8/ConsoleApp1/ClassCharacter.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WW
+{
+    enum Race
+    {
+        Elf,
+        Ork,
+        Gnum,
+        Human
+    }
+    enum Gender
+    {
+        Male,
+        Female
+    }
+    interface ILvl
+    {
+
+        void LvLUp(int a);
+    }
+    class Character : IComparable<Character>
+    {
+        protected int age;
+        protected Race race;
+        protected string name;
+        protected Gender gender;
+        protected static int nextID = 0;
+        public int ID { get; protected set; }
+        public Character() { }
+        public Character(int _age, Race _race, string _name, Gender _gender)
+        {
+            ID = ++nextID;
+            age = _age;
+            name = _name;
+            gender = _gender;
+            race = _race;
+        }
+        public Character(ref Character A)
+        {
+            ID = ++nextID;
+            age = A.age;
+            name = A.name;
+            gender = A.gender;
+        }
+        ~Character()
+        {
+            Console.WriteLine("Удалено");
+        }
+        public int CompareTo(Character obj)
+        {
+            if (age > obj.age)
+            {
+                return 1;
+            }
+            if (age == obj.age)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (age < 0)
+                {
+                    throw new Exception("Возраст меньше нуля");
+                }
+                else
+                {
+                    age = value;
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name.Length == 0)
+                {
+                    throw new Exception("Пустая строка");
+                }
+                if (name == null)
+                {
+                    throw new Exception("Нулевой указатель на строку");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+        public Gender Ggender
+        {
+            get
+            {
+                return gender;
+            }
+            set
+            {
+                gender = value;
+            }
+        }
+        public Race Rrace
+        {
+            get
+            {
+                return race;
+            }
+            set
+            {
+                race = value;
+            }
+        }
+        public static bool operator ==(Character A, Character B)
+        {
+            return (A.age == B.age && A.name == B.name && A.gender == B.gender);
+        }
+        public static bool operator !=(Character A, Character B)
+        {
+            return !(A == B);
+        }
+    }
+}
---
+++ last 3,5,6,8/ConsoleApp1/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WW
+{
+    delegate void Fight(string delegatestring);
+    class Program
+    {
+        public static void OnEnded(object sender, EventArgs eventArgs)
+        {
+            if (sender is Necromant)
+            {
+                Console.WriteLine("Некроманту не хватает стамины");
+            }
+            else if (sender is Mage)
+            {
+                Console.WriteLine("Магу не хватает маны");
+            }
+        }
+        public static void NecroWin(string somestring)
+        {
+            Console.WriteLine(somestring);
+        }
+        public static void MageWin(string somestr)
+        {
+            Console.WriteLine(somestr);
+        }
+        public static void Battle(Mage A, Necromant B, Fight n, Fight m)
+        {
+            Console.WriteLine("Маг и Некромант сошлись в битве заклинаний");
+            Console.WriteLine("Исход битвы:");
+            if (A.Mana > B.Mana)
+            {
+                m("у Мага больше маны на => он выиграл\n");
+                Console.WriteLine("Разница в мане на {0}\n", A.Mana - B.Mana);
+            }
+            else if (B.Mana > A.Mana)
+            {
+                n("у Некроманта больше маны на => он выиграл\n");
+                Console.WriteLine("Разница в мане на {0}\n", B.Mana - A.Mana);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Character FirstObject = new Character(18, Race.Human, "Kirill", Gender.Male);
+            Character SecondObject = new Character(ref FirstObject);
+            Character ThirdObject = new Character(54, Race.Ork, "Valerii", Gender.Male);
+            Mage FourthObject = new Mage(154, Race.Elf, "Avallakh", Gender.Male, 70, 100, 1);
+            Necromant FifthObject = new Necromant(100, Race.Ork, "Guldan", Gender.Male, 80, 150, 60, 100, 1);
+            Fight fightresult = NecroWin;
+            Fight fightresult2 = MageWin;
+            Battle(FourthObject, FifthObject, fightresult, fightresult2);
+            ILvl l = FourthObject;
+            ILvl l2 = FifthObject;
+            var characters = new List<Mage>
+            {
+               FourthObject,
+               FifthObject
+            };
+            List<Character> age = new List<Character>()
+            {
+                FirstObject,
+                ThirdObject,
+                FourthObject,
+                FifthObject
+            };
+            DelegateRevive method = FifthObject.Revive;
+            Console.WriteLine("First Object: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\n", FirstObject.Age, FirstObject.Name, FirstObject.Ggender, FirstObject.Rrace);
+            Console.WriteLine("Third Object: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\n", ThirdObject.Age, ThirdObject.Name, ThirdObject.Ggender, ThirdObject.Rrace);
+            Console.WriteLine("Fourth Object: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nТекущий уровень: {6}\n", FourthObject.Age, FourthObject.Name, FourthObject.Ggender,
+            FourthObject.Rrace, FourthObject.Mana, FourthObject.MaxMana, FourthObject.Lvl);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\nТекущий уровень: {9}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat, FifthObject.Lvl);
+
+            Console.WriteLine("FirstObject == SecondObject - {0}", FirstObject == SecondObject);
+            Console.WriteLine("SecondObject == ThirdObject - {0}", SecondObject == ThirdObject);
+            Console.WriteLine("FirstObject == ThirdObject - {0}\n", FirstObject == ThirdObject);
+
+            Console.WriteLine("Использование способности ChangeAge(-1 год) магом: ");
+            FourthObject.ChangeAge(1);
+            Console.WriteLine("Fourth Object: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\n", FourthObject.Age, FourthObject.Name, FourthObject.Ggender,
+            FourthObject.Rrace, FourthObject.Mana, FourthObject.MaxMana);
+
+            Console.WriteLine("Использование способности Revive(5 душ)\n");
+            FifthObject.Revive(5);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat);
+            Console.WriteLine("Использование способности ChangeAge(3 года)");
+            FifthObject.ChangeAge(3);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat);
+            Console.WriteLine("Использование способности Revive (5 душ)\n");
+            method(5); // вызов метода Revive через объект делегата
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat);
+            l2.LvLUp(5);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\nТекущий уровень: {9}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat, FifthObject.Lvl);
+            Console.WriteLine("FirstObject ID = {0}, SecondObject ID = {1}, ThirdObject ID = {2}, FourthObject ID = {3}\n", FirstObject.ID, SecondObject.ID, ThirdObject.ID, FourthObject.ID);
+            foreach (var charr in characters)
+            {
+                charr.ChangeAge(3);
+            }
+            l.LvLUp(5);
+            Console.WriteLine("Fourth Object: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\n", FourthObject.Age, FourthObject.Name, FourthObject.Ggender,
+           FourthObject.Rrace, FourthObject.Mana, FourthObject.MaxMana);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nВыносливость: {6}\nМаксимальная выносливость: {7}\nСостояние: {8}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FifthObject.Rrace, FifthObject.Mana, FifthObject.MaxMana, FifthObject.Stamina, FifthObject.MaxStamina, FifthObject.Stat);
+            foreach (Character x in age)
+            {
+                Console.WriteLine("Имя: {0}\tВозраст: {1}", x.Name, x.Age);
+            }
+            age.Sort();
+            Console.WriteLine("\n");
+            foreach (Character x in age)
+            {
+                Console.WriteLine("Имя: {0}\tВозраст: {1}", x.Name, x.Age);
+            }
+            FourthObject.LvlUpChangeAge(2, 5);
+            Console.WriteLine("FifthObject: \nВозраст: {0}\nИмя: {1}\nПол: {2}\nРаса: {3}\nМана: {4}\nМаксимальная мана: {5}\nТекущий уровень: {6}\n", FifthObject.Age, FifthObject.Name, FifthObject.Ggender,
+            FourthObject.Rrace, FourthObject.Mana, FourthObject.MaxMana, FourthObject.Lvl);
+
+            FourthObject.Ended += OnEnded;  // подписка на один и тот же метод, но с разделением у какого класса сработало
+            FifthObject.Ended += OnEnded;
+
+            FourthObject.ManaWasteForTest();
+            FifthObject.StmWasteForTest();
+
+            Console.ReadKey();
+        }
+    }
+}
+
